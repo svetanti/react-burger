3caadc27fabf59d47110933a969946526f1ed044
Svetlana Antipova <svetlanaantipova@Svetlanas-MacBook-Air.local>
2022-02-04 15:53:00 +0300

    reginster and login page

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './login-page.module.css';
+import Form from '../components/form/form';
 
 function LoginPage() {
-  return <div>Login</div>;
+  const fields = [
+    { name: 'email', placeholder: 'E-mail' },
+    { name: 'password', placeholder: 'Пароль' },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Вход</h1>
+      <Form
+        fields={fields}
+        buttonText="Войти"
+      />
+      <p className={styles.text}>
+        Вы — новый пользователь?
+        <Link to="/register" className={styles.link}>Зарегистрироваться</Link>
+      </p>
+      <p className={styles.text}>
+        Забыли пароль?
+        <Link to="/forgot-password" className={styles.link}>Восстановить пароль</Link>
+      </p>
+    </div>
+  );
 }
 
 export default LoginPage;
--- src/pages/register-page.jsx
+++ src/pages/register-page.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Form from '../components/form/form';
+import styles from './login-page.module.css';
 
 function RegisterPage() {
-  return <div>Register</div>;
+  const fields = [
+    { name: 'name', placeholder: 'Имя' },
+    { name: 'email', placeholder: 'E-mail' },
+    { name: 'password', placeholder: 'Пароль' },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Регистрация</h1>
+      <Form
+        fields={fields}
+        buttonText="Зарегистрироваться"
+      />
+      <p className={styles.text}>
+        Уже зарегистрированы?
+        <Link to="/login" className={styles.link}>Войти</Link>
+      </p>
+    </div>
+  );
 }
 
 export default RegisterPage;
