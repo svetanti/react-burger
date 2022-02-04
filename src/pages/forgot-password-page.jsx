4595916ee9c1e4b79db59962b6dd390c8bdb87fd
Svetlana Antipova <svetlanaantipova@Svetlanas-MacBook-Air.local>
2022-02-04 15:58:00 +0300

    reset-password and forgot-password pages

--- src/pages/forgot-password-page.jsx
+++ src/pages/forgot-password-page.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './login-page.module.css';
+import Form from '../components/form/form';
 
 function ForgotPasswordPage() {
-  return <div>Forgot Password</div>;
+  const fields = [
+    { name: 'email', placeholder: 'Укажите e-mail' },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Восстановление пароля</h1>
+      <Form
+        fields={fields}
+        buttonText="Восстановить"
+      />
+      <p className={styles.text}>
+        Вспомнили пароль?
+        <Link to="/login" className={styles.link}>Войти</Link>
+      </p>
+    </div>
+  );
 }
 
 export default ForgotPasswordPage;
--- src/pages/reset-password.jsx
+++ src/pages/reset-password.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Form from '../components/form/form';
+import styles from './login-page.module.css';
 
 function ResetPasswordPage() {
-  return <div>Reset Password</div>;
+  const fields = [
+    { name: 'password', placeholder: 'Введите новый пароль' },
+    { name: 'text', placeholder: 'Введите код из письма' },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Восстановление пароля</h1>
+      <Form
+        fields={fields}
+        buttonText="Сохранить"
+      />
+      <p className={styles.text}>
+        Вспомнили пароль?
+        <Link to="/login" className={styles.link}>Войти</Link>
+      </p>
+    </div>
+  );
 }
 
 export default ResetPasswordPage;
